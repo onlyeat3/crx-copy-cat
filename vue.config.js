572756eff14ce02d63d23a785a0f6fbe2b142bda
onlyeat3 <onlyeat3@gmail.com>
2021-10-12 21:08:55 +0800

    fix 打包后丢失css文件

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,31 @@
 module.exports = {
+  filenameHashing: false,
   lintOnSave: false,
   pages: {},
   pluginOptions: {
     browserExtension: {
+      components: {
+        contentScripts: true
+      },
       componentOptions: {
         contentScripts: {
           entries: {
-            'content-script': [
+            'content-embed': [
               'src/content-scripts/content-script.js'
             ]
           }
         }
+      },
+      manifestTransformer: (manifest) => {
+        delete manifest.content_security_policy
+        // manifest.content_security_policy = {
+        //  "extension_pages": manifest.content_security_policy.replace('script-src', 'script-src-elem http://localhost:8098')
+        // }
+        return manifest;
       }
     }
+  },
+  configureWebpack: {
+    devtool: "true",
   }
 }
